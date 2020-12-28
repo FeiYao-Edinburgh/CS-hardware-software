91ace06724c12e71bd7d206aa487c1dff67ad73a
FeiYao-Edinburgh <fei.yao@ed.ac.uk>
2020-12-28 17:04:07 +0000

    sweep python subdir

--- Python/Scripts/sys_stdout_write.py
+++
@@ -1,7 +0,0 @@
-﻿import time,sys
-for i in range(1,100,1):
-    sys.stdout.write("\r{0}{1:.2f}%".format("|"*i,i))
-    # Or simply use sys.stdout.write(f"\r{i*'|'}{i:0.2f}%") for the above.
-    # '\r' will send the cursor to the beginning of the line, where it can overwrite the existing text, which also means the later text should have more characters than the former ones otherwise unwanted overlap will emerge.
-    sys.stdout.flush() # For better effect.
-    time.sleep(0.01)
